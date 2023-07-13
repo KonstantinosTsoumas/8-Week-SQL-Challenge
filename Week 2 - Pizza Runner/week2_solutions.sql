0e33c207df5cc03f71a514cbefb88db2f5100833
Konstantinos <tsoumascon@gmail.com>
2023-07-13 23:09:57 +0200

    add solution for question 2

--- Week 2 - Pizza Runner/week2_solutions.sql
+++ Week 2 - Pizza Runner/week2_solutions.sql
@@ -41,3 +41,17 @@ SELECT
 	  ELSE cancellation
 	  END AS cancellation
 FROM pizza_runner.runner_orders;
+
+--1. How many pizzas were ordered?
+SELECT COUNT(*) AS count_orders
+FROM customer_orders_cleaned;
+
+--2. How many unique customer orders were made?
+SELECT COUNT(DISTINCT order_id) AS unique_no_of_orders
+FROM customer_orders_cleaned;
+--3. How many successful orders were delivered by each runner?
+SELECT runner_id,
+	COUNT(order_id) AS delivered_orders
+FROM runner_orders_cleaned
+where cancellation LIKE ' '
+GROUP BY runner_id;
