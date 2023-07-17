c3622c25a4211777be4cba5c7ae8d6db63f78a8e
Konstantinos <tsoumascon@gmail.com>
2023-07-17 22:01:08 +0200

    solutions for question 4

--- Week 4 - Data Bank/data_bank.sql
+++ Week 4 - Data Bank/data_bank.sql
@@ -20,3 +20,24 @@ FROM data_bank.customer_nodes AS nodes
 JOIN data_bank.regions AS regions ON nodes.region_id = regions.region_id
 GROUP BY regions.region_id, region_name;
 
+--4.How many days on average are customers reallocated to a different node?
+WITH number_of_days_cte AS (
+  SELECT
+  	customer_id,
+  	node_id,
+    end_date - start_date AS amount_of_days_in_node
+  FROM data_bank.customer_nodes
+  WHERE end_date!='9999-12-31'
+  ),
+  	total_number_of_days AS (
+    SELECT
+      	customer_id,
+      	node_id,
+      	SUM(amount_of_days_in_node) AS total_days_in_node
+    FROM number_of_days_cte
+    GROUP BY customer_id, node_id
+	)
+
+
+SELECT ROUND(AVG(total_days_in_node),1) AS avg_reallocated_days
+FROM total_number_of_days
