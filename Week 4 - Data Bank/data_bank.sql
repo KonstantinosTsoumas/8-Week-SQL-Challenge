852718459db56db4dd1534ee820370ba62083e9e
Konstantinos <tsoumascon@gmail.com>
2023-07-17 22:00:59 +0200

    solutions for question 3

--- Week 4 - Data Bank/data_bank.sql
+++ Week 4 - Data Bank/data_bank.sql
@@ -10,3 +10,13 @@ SELECT
 FROM data_bank.customer_nodes AS nodes
 JOIN data_bank.regions AS regions ON nodes.region_id = regions.region_id
 GROUP BY regions.region_id, region_name;
+
+--3. How many customers are allocated to each region?
+SELECT 
+	regions.region_id,
+    region_name,
+	COUNT(DISTINCT customer_id) AS customers_count
+FROM data_bank.customer_nodes AS nodes
+JOIN data_bank.regions AS regions ON nodes.region_id = regions.region_id
+GROUP BY regions.region_id, region_name;
+
