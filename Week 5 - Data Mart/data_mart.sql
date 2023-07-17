a6cea5199f91c9a66418dac32efa70153f8aaf0f
Konstantinos <tsoumascon@gmail.com>
2023-07-17 22:53:17 +0200

    add solution to the second question

--- Week 5 - Data Mart/data_mart.sql
+++ Week 5 - Data Mart/data_mart.sql
@@ -1,3 +1,17 @@
 --1. What day of the week is used for each week_date value?
 SELECT DISTINCT(TO_CHAR(week_date, 'day')) AS week_day 
 FROM clean_weekly_sales;
+
+--2. What range of week numbers are missing from the dataset?
+WITH week_number_cte AS (
+  SELECT GENERATE_SERIES(1, 52) AS week_number
+),
+existing_weeks AS (
+  SELECT week_number
+  FROM clean_weekly_sales
+)
+SELECT week_number
+FROM week_number_cte
+EXCEPT
+SELECT week_number
+FROM existing_weeks;
