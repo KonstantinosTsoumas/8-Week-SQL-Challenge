85b232a01ffb04d34ff36dd2368cebf14ca063f6
Konstantinos <tsoumascon@gmail.com>
2023-07-17 21:46:39 +0200

    solutions for question 2

--- Week 4 - Data Bank/data_bank.sql
+++ Week 4 - Data Bank/data_bank.sql
@@ -1,3 +1,12 @@
 --1.How many unique nodes are there on the Data Bank system?
 SELECT count(DISTINCT node_id) AS unique_nodes
 FROM customer_nodes;
+
+--2.What is the number of nodes per region?
+SELECT 
+	regions.region_id,
+    region_name,
+	COUNT(nodes.node_id) AS nodes_count
+FROM data_bank.customer_nodes AS nodes
+JOIN data_bank.regions AS regions ON nodes.region_id = regions.region_id
+GROUP BY regions.region_id, region_name;
