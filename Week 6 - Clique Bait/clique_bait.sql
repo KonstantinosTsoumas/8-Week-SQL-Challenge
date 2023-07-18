c06182a2489879a05a35ff1a6970d2a418cb5695
Konstantinos <tsoumascon@gmail.com>
2023-07-18 23:05:33 +0200

    Added query to find the product which had the highest view to purchase percentage. Used the newly created product_info table to answer the question

--- Week 6 - Clique Bait/clique_bait.sql
+++ Week 6 - Clique Bait/clique_bait.sql
@@ -6,31 +6,52 @@
 -- How many times was each product purchased?
 
 -- The Single Query answer to the above.
-WITH combined_events AS (
+WITH product_events AS (
   SELECT
     events.visit_id,
-    page_hierarchy.product_id AS product_id,
+    page_hierarchy.product_id,
     page_hierarchy.page_name AS product_name,
     page_hierarchy.product_category,
     SUM(CASE WHEN events.event_type = 1 THEN 1 ELSE 0 END) AS page_view,
-    SUM(CASE WHEN events.event_type = 2 THEN 1 ELSE 0 END) AS cart_add,
-    MAX(CASE WHEN events.event_type = 3 THEN 1 ELSE 0 END) AS purchase
+    SUM(CASE WHEN events.event_type = 2 THEN 1 ELSE 0 END) AS cart_add 
   FROM clique_bait.events AS events
   JOIN clique_bait.page_hierarchy AS page_hierarchy
     ON events.page_id = page_hierarchy.page_id
   WHERE product_id IS NOT NULL
   GROUP BY events.visit_id, page_hierarchy.product_id, page_hierarchy.page_name, page_hierarchy.product_category
 ),
+
+purchase_events AS (
+  SELECT
+    DISTINCT visit_id
+  FROM clique_bait.events
+  WHERE event_type = 3 
+),
+
+combined_table AS (
+  SELECT
+    product_events.visit_id,
+    product_events.product_id,
+    product_events.product_name,
+    product_events.product_category,
+    product_events.page_view,
+    product_events.cart_add,
+    CASE WHEN purchase_events.visit_id IS NOT NULL THEN 1 ELSE 0 END AS purchase
+  FROM product_events
+  LEFT JOIN purchase_events
+    ON product_events.visit_id = purchase_events.visit_id
+),
+
 product_info AS (
-  SELECT 
+  SELECT
     product_id,
-    product_name, 
-    product_category, 
+    product_name,
+    product_category,
     SUM(page_view) AS views,
-    SUM(cart_add) AS cart_adds, 
+    SUM(cart_add) AS cart_adds,
     SUM(CASE WHEN cart_add = 1 AND purchase = 0 THEN 1 ELSE 0 END) AS abandoned,
     SUM(CASE WHEN cart_add = 1 AND purchase = 1 THEN 1 ELSE 0 END) AS purchases
-  FROM combined_events
+  FROM combined_table
   GROUP BY product_id, product_name, product_category
 )
 
@@ -113,4 +134,11 @@ ORDER BY abandoned DESC
 LIMIT 1;
 
 -- 3. Which product had the highest view to purchase percentage?
-
+-- This answer should be embeded to the first single (big) query.
+SELECT 
+  product_name, 
+  product_category, 
+  ROUND((100 * purchases) / views, 2) AS percentage_purchase_per_view
+FROM product_info
+ORDER BY percentage_purchase_per_view DESC
+LIMIT 1;
