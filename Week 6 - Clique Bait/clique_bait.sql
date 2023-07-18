1183ec7ebf2a69aec84c7605a004b63e6216928a
Konstantinos <tsoumascon@gmail.com>
2023-07-18 22:58:16 +0200

    modified aggregated SQL query to account for purchases

--- Week 6 - Clique Bait/clique_bait.sql
+++ Week 6 - Clique Bait/clique_bait.sql
@@ -40,47 +40,50 @@ ORDER BY product_id;
 
 
 -- Additionally, create another table which further aggregates the data for the above points but this time for each product category instead of individual products.
-WITH combined_events AS (
-  SELECT
+WITH product_events AS ( 
+  SELECT 
     events.visit_id,
-    page_hierarchy.product_id AS product_id,
+    page_hierarchy.product_id,
     page_hierarchy.page_name AS product_name,
     page_hierarchy.product_category,
     SUM(CASE WHEN events.event_type = 1 THEN 1 ELSE 0 END) AS page_view,
-    SUM(CASE WHEN events.event_type = 2 THEN 1 ELSE 0 END) AS cart_add,
-    MAX(CASE WHEN events.event_type = 3 THEN 1 ELSE 0 END) AS purchase
+    SUM(CASE WHEN events.event_type = 2 THEN 1 ELSE 0 END) AS cart_add 
   FROM clique_bait.events AS events
   JOIN clique_bait.page_hierarchy AS page_hierarchy
     ON events.page_id = page_hierarchy.page_id
   WHERE product_id IS NOT NULL
   GROUP BY events.visit_id, page_hierarchy.product_id, page_hierarchy.page_name, page_hierarchy.product_category
 ),
-product_info AS (
+purchase_events AS ( 
   SELECT 
-    product_id,
-    product_name, 
+    DISTINCT visit_id
+  FROM clique_bait.events
+  WHERE event_type = 3 
+),
+combined_table AS (
+  SELECT 
+    product_events.visit_id, 
+    product_events.product_id, 
+    product_events.product_name, 
+    product_events.product_category, 
+    product_events.page_view, 
+    product_events.cart_add,
+    CASE WHEN purchase_events.visit_id IS NOT NULL THEN 1 ELSE 0 END AS purchase
+  FROM product_events
+  LEFT JOIN purchase_events
+    ON product_events.visit_id = purchase_events.visit_id
+),
+product_category AS (
+  SELECT 
     product_category, 
     SUM(page_view) AS views,
     SUM(cart_add) AS cart_adds, 
     SUM(CASE WHEN cart_add = 1 AND purchase = 0 THEN 1 ELSE 0 END) AS abandoned,
     SUM(CASE WHEN cart_add = 1 AND purchase = 1 THEN 1 ELSE 0 END) AS purchases
-  FROM combined_events
-  GROUP BY product_id, product_name, product_category
-),
-category_info AS (
-  SELECT
-    product_category,
-    SUM(views) AS total_views,
-    SUM(cart_adds) AS total_cart_adds,
-    SUM(abandoned) AS total_abandoned,
-    SUM(purchases) AS total_purchases
-  FROM product_info
-  GROUP BY product_category
-)
-
+  FROM combined_table
+  GROUP BY product_category)
 SELECT *
-FROM category_info
-ORDER BY product_category;
+FROM product_category
 
 -- Use your 2 new output tables - answer the following questions:
 
@@ -108,3 +111,6 @@ SELECT *
 FROM product_info
 ORDER BY abandoned DESC
 LIMIT 1;
+
+-- 3. Which product had the highest view to purchase percentage?
+
