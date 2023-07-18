ec9eccde1ba13a4acfbf6b40a6c65fbe0ad6a06b
Konstantinos <tsoumascon@gmail.com>
2023-07-18 22:22:06 +0200

    answer multiple question with a single query such as times viewed, added to cart or purchased

--- Week 6 - Clique Bait/clique_bait.sql
+++ Week 6 - Clique Bait/clique_bait.sql
@@ -1,4 +1,39 @@
---1. How many users are there?
-SELECT 
-  COUNT(DISTINCT user_id) AS user_count
-FROM clique_bait.users;
+--Using a single SQL query - create a new output table which has the following details:
+
+--How many times was each product viewed?
+--How many times was each product added to cart?
+--How many times was each product added to a cart but not purchased (abandoned)?
+--How many times was each product purchased?
+
+--The Single Query answer to the above.
+WITH combined_events AS (
+  SELECT
+    events.visit_id,
+    page_hierarchy.product_id AS product_id,
+    page_hierarchy.page_name AS product_name,
+    page_hierarchy.product_category,
+    SUM(CASE WHEN events.event_type = 1 THEN 1 ELSE 0 END) AS page_view,
+    SUM(CASE WHEN events.event_type = 2 THEN 1 ELSE 0 END) AS cart_add,
+    MAX(CASE WHEN events.event_type = 3 THEN 1 ELSE 0 END) AS purchase
+  FROM clique_bait.events AS events
+  JOIN clique_bait.page_hierarchy AS page_hierarchy
+    ON events.page_id = page_hierarchy.page_id
+  WHERE product_id IS NOT NULL
+  GROUP BY events.visit_id, page_hierarchy.product_id, page_hierarchy.page_name, page_hierarchy.product_category
+),
+product_info AS (
+  SELECT 
+    product_id,
+    product_name, 
+    product_category, 
+    SUM(page_view) AS views,
+    SUM(cart_add) AS cart_adds, 
+    SUM(CASE WHEN cart_add = 1 AND purchase = 0 THEN 1 ELSE 0 END) AS abandoned,
+    SUM(CASE WHEN cart_add = 1 AND purchase = 1 THEN 1 ELSE 0 END) AS purchases
+  FROM combined_events
+  GROUP BY product_id, product_name, product_category
+)
+
+SELECT *
+FROM product_info
+ORDER BY product_id;
