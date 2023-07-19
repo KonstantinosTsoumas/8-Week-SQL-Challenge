e5e90bfaba9a9345a5a80ac7dd909f5dd95a4630
Konstantinos Tsoumas <95776539+KonstantinosTsoumas@users.noreply.github.com>
2023-07-19 08:54:33 +0200

    Found the average unique products purchased in each transaction using the sales table

--- Week 7 - Balanced Tree Clothing Co./transaction_analysis.sql
+++ Week 7 - Balanced Tree Clothing Co./transaction_analysis.sql
@@ -1,3 +1,14 @@
---1. How many unique transactions were there? 
+-- 1. How many unique transactions were there? 
 SELECT COUNT(DISTINCT txn_id) AS unique_transactions
 FROM balanced_tree.sales;
+
+-- 2. What is the average unique products purchased in each transaction?
+SELECT 
+	ROUND(AVG(product_count),2)
+    FROM (
+SELECT
+	txn_id,
+    COUNT(DISTINCT prod_id) AS product_count
+FROM balanced_tree.sales
+GROUP BY txn_id
+      )product_query ; 
