e554295793a3822f76fa54f0e5bc29af1beb1eb7
Konstantinos <tsoumascon@gmail.com>
2023-07-21 08:45:51 +0200

    Find how many records are there for each 'month_year'

--- Week 8 - Fresh Segments/data_cleaning.sql
+++ Week 8 - Fresh Segments/data_cleaning.sql
@@ -1,3 +1,9 @@
 -- 1. Update the fresh_segments.interest_metrics table by modifying the month_year column to be a date data type with the start of the month
 ALTER TABLE fresh_segments.interest_metrics
 ALTER COLUMN month_year TYPE DATE USING TO_DATE(month_year, 'MM-YYYY');
+
+-- 2. What is count of records in the fresh_segments.interest_metrics for each month_year value sorted in chronological order (earliest to latest) with the null values appearing first?
+SELECT month_year, COUNT(*) AS record_count
+FROM fresh_segments.interest_metrics
+GROUP BY month_year
+ORDER BY month_year NULLS FIRST;
