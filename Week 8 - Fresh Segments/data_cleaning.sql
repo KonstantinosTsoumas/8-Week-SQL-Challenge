bee6994d9c9cf664014766d9f9b0657e10ef304a
Konstantinos <tsoumascon@gmail.com>
2023-07-21 08:42:30 +0200

    transform 'month_year' column to date format

---
+++ Week 8 - Fresh Segments/data_cleaning.sql
@@ -0,0 +1,3 @@
+-- 1. Update the fresh_segments.interest_metrics table by modifying the month_year column to be a date data type with the start of the month
+ALTER TABLE fresh_segments.interest_metrics
+ALTER COLUMN month_year TYPE DATE USING TO_DATE(month_year, 'MM-YYYY');
