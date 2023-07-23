85c626940b17e1c6290db7d03d005fb19a211700
Konstantinos <tsoumascon@gmail.com>
2023-07-23 20:55:59 +0200

    found the average of the average composition

--- Week 8 - Fresh Segments/fresh_segments.sql
+++ Week 8 - Fresh Segments/fresh_segments.sql
@@ -38,3 +38,21 @@ WHERE rank_per_month <= 10
 GROUP BY interest_name
 ORDER BY appearance_count DESC
 LIMIT 5;
+
+-- 3. What is the average of the average composition for the top 10 interests for each month?
+WITH composion_average_ranking_cte AS (
+  SELECT 
+    metrics.interest_id,
+    map.interest_name,
+    metrics.month_year,
+	ROUND(CAST(metrics.composition AS NUMERIC) / CAST(metrics.index_value AS NUMERIC), 2) AS avg_composition,
+    DENSE_RANK() OVER(PARTITION BY metrics.month_year ORDER BY metrics.composition / metrics.index_value DESC) AS rank_per_month
+  FROM fresh_segments.interest_metrics metrics
+  JOIN fresh_segments.interest_map map 
+    ON CAST(metrics.interest_id AS INTEGER) = map.id
+  WHERE metrics.month_year IS NOT NULL
+) 
+SELECT month_year, AVG(avg_composition) AS average_composition
+FROM composion_average_ranking_cte
+WHERE rank_per_month <= 10
+GROUP BY month_year;
